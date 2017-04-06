7074a016b58cb36a661ef663464094620f1e17b6
Rishabh <rishabh@hypertrack.io>
2017-04-06 15:51:16 +0530

    Updates to dist

--- dist/src/helpers.d.ts
+++ dist/src/helpers.d.ts
@@ -1,5 +1,4 @@
 /// <reference types="googlemaps" />
-/// <reference types="jquery" />
 import { IPlace } from "./model";
 export declare const GetBaseUrl: (env?: string) => string;
 export declare function GetReqOpt(pk: string): {
@@ -9,5 +8,5 @@ export declare function GetReqOpt(pk: string): {
     };
 };
 export declare function GetLatLng(place: IPlace | any, key?: string): google.maps.LatLng;
-export declare function FetchAction(actionId: string, pk: string): JQueryXHR;
+export declare function FetchAction(actionId: string, pk: string): any;
 export declare function SetMap(item: any, map: any): void;
