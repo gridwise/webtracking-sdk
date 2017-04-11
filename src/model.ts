2b3095e1a5cf160e5d404aa1d12b1e1906489d92
Rishabh <rishabh@hypertrack.io>
2017-04-12 01:29:29 +0530

    Add photo to user model

--- src/model.ts
+++ src/model.ts
@@ -43,6 +43,7 @@ export interface IUser {
     name: string,
     phone: number | string | null,
     last_heartbeat_at: string | null,
+    photo: string,
     last_location: {
         geojson: IPlace
     }
